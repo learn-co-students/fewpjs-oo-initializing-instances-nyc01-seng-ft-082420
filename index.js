5d5a580ac9dd0390e4e5b4746796d17405eaaa6b
jdhofmann <justindhofmann@gmail.com>
2020-10-17 12:58:54 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here
+class Dog {
+  constructor(name, breed){
+    this.name = name;
+    this.breed = breed;
+  }
+}
+
+let tomTheDog = new Dog("Tom", "black");
+let zarroTheDog = new Dog("Zarro", "gray");
+
+class Breakfast {
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+class Lunch {
+  constructor(soup, salad){
+    this.soup = soup;
+    this.salad = salad;
+  }
+}
+class Dinner {
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
